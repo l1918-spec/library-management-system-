2745329653f30fb795da707742fc7a81f20c3f17
Lydia Charif <80354370+l1918-spec@users.noreply.github.com>
2024-09-11 01:33:55 +0100

    Update custom.js

--- library/admin/assets/js/custom.js
+++ library/admin/assets/js/custom.js
@@ -1,14 +1,4 @@
-﻿
-/*=============================================================
-    Authour URI: www.binarytheme.com
-    License: Commons Attribution 3.0
 
-    http://creativecommons.org/licenses/by/3.0/
-
-    100% Free To use For Personal And Commercial Use.
-    IN EXCHANGE JUST GIVE US CREDITS AND TELL YOUR FRIENDS ABOUT US
-   
-    ========================================================  */
 
 (function ($) {
     "use strict";
@@ -16,7 +6,7 @@
         slide_fun: function () {
 
             $('#carousel-example').carousel({
-                interval:3000 // THIS TIME IS IN MILLI SECONDS
+                interval:3000 // ms
             })
 
         },
@@ -28,11 +18,6 @@
        
         custom_fun:function()
         {
-            /*====================================
-             WRITE YOUR   SCRIPTS  BELOW
-            ======================================*/
-
-
 
 
         },
